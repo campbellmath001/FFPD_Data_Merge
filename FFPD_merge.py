b703cbfe0c2602f8fa7a2b85abcf5d3ec6e58512
Leatherback <Leatherback@SolanoLoggingTurtles>
2024-07-15 09:15:54 -0700

    data cleaned in pandas and ready for merge

--- FFPD_merge.py
+++ FFPD_merge.py
@@ -4,13 +4,27 @@ import pandas as pd
 # Path to folder containing the csv files to merge
 q = Path('./FFPD_Data')
 
-
 csv_files = q.glob('*.csv')
 
 # create a list of dataframes from the csv files
 data = [pd.read_csv(f) for f in csv_files]
 
 raw_df = pd.concat(data, ignore_index=True).drop(['Unnamed: 0'],axis = 1)
+
+df = (
+        raw_df.rename(
+            columns={'Date Time':'Incident_Time', 'inci #:':'Incident_Id'}
+        )
+        .assign(
+            Incident_Time=lambda x: pd.to_datetime(x['Incident_Time'], format='%m/%d/%Y %I:%M:%S %p'),
+            Block=lambda x: x['Address'].str.extract(r'(\d+) Block of (?P<street>.+)')[0],
+            Street=lambda x: x['Address'].str.extract(r'(\d+) Block of (?P<street>.+)')['street'],
+            Full_Address=lambda x: x['Block'].astype(str) + ' ' + x['Street'] + ' Fairfield, Ca'
+        )
+        .sort_values(
+            by='Incident_Time'
+        )
+)
 
 
 
